84a4bc6fe0f4750cf65b55bbd3ab02494ba1a7ab
Qian Molloy <65090352+qianzmolloy@users.noreply.github.com>
2025-01-12 21:11:17 -0500

    Create diffdock.sh

---
+++ diffdock.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script: diffdock.sh - Run inference using local files as input
+# Usage: ./diffdock.sh [receptor].pdb [ligand].sdf
+
+protein_file=$1
+ligand_file=$2
+
+protein_bytes=`grep -E ^ATOM $protein_file | sed -z 's/\n/\\\n/g'`
+ligand_bytes=`sed -z 's/\n/\\\n/g' $ligand_file`
+ligand_format=`basename $ligand_file | awk -F. '{print $NF}'`
+
+echo "{
+\"ligand\": \"${ligand_bytes}\",
+\"ligand_file_type\": \"${ligand_format}\",
+\"protein\": \"${protein_bytes}\",
+\"num_poses\": 10,
+\"time_divisions\": 20,
+\"steps\": 18,
+\"save_trajectory\": false,
+\"is_staged\": false
+}" > diffdock.json
+
+curl --header "Content-Type: application/json" \
+   --request POST \
+   --data @diffdock.json \
+   --output output.json \
+   http://localhost:8000/molecular-docking/diffdock/generate
